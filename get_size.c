60793b3c7e39cf2a3415cba5e1404e5d6cf152e5
Jerima07 <90107703+Jerima07@users.noreply.github.com>
2022-08-09 11:59:00 +0000

    Update get_size.c
    Calculate the size of arg

--- get_size.c
+++ get_size.c
@@ -3,7 +3,7 @@
 /**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed
+ * @i: a list of arguments to be printed
  *
  * Return: Precision.
  */
