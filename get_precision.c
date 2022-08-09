888a3e73e58b79ad2bc5c3a76f243c61dd47b106
Jerima07 <90107703+Jerima07@users.noreply.github.com>
2022-08-09 13:43:41 +0000

    Update get_precision.c
    Calculate print precision

--- get_precision.c
+++ get_precision.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_precision - function to calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed
  * @list: list of arguments
